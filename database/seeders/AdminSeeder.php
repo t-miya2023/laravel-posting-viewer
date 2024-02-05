f95b71749e1a610100aa0a94d74df1769239db15
t-miya2023 <t.miyata2023@gmail.com>
2024-02-05 15:46:05 +0900

    3

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'テストユーザー',
+            'email' => 'test@test.com',
+            'password' => bcrypt('password123'),
+        ]);
+    }
+}
